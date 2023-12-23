e605b7f3240db73783af2f34478c6536cc16cb6a
Steve Appeltants <steve.appeltants@student.pxl.be>
2023-12-23 02:35:37 +0100

    Update playwright.test.ts

--- e2e/playwright.test.ts
+++ e2e/playwright.test.ts
@@ -1,15 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-    test('App has a button', async ({ page }) => {
-    await page.goto('http://localhost:3000');
+test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+  });
 
+    test('App has a button', async ({ page }) => {
     const addButton = await page.waitForSelector('#add');
     expect(addButton).toBeTruthy();
 });
 
-test('App can cubstract', async ({ page }) => {
-    await page.goto('http://localhost:3000');
-
+    test('App can cubstract', async ({ page }) => {
     const subtractButton = await page.locator('[role="button"][name="subtract"]');
 
     await page.fill('#number1', '3');
